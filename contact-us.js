48b87b9b619697e151e89b4f55d03299da1d6ded
Thabiso Matsaba <Thabisomatsaba@capaciti.org.za>
2023-08-30 13:39:09 +0200

    Done with json-server  fake api database

--- contact-us.js
+++ contact-us.js
@@ -1,37 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Declare form values for the contact us form
+    const form = document.getElementById('form');
 
-// Delcare form values for contact us form
-const formContact = document.querySelector('.form');
-if(formContact){
-    formContact.addEventListener('click', swapper, false);
-}
-// const name = document.getElementById('name');
-// const email = document.getElementById('email');
-// const message = document.getElementById('message');
+    // Add an EventListener to get user inputs 
+    form.addEventListener('submit', event => {
+        event.preventDefault();
 
-// Add an EventListener to get user inputs 
-formContact.addEventListener('submit', event => {
-    event.preventDefault();
+        const formData = new FormData(form);
+        const data = Object.fromEntries(formData);
 
-    const formData = new FormData(formContact);
-    const data = Object.fromEntries(formData)
-
-    fetch('http://localhost:3000/users', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    });
-
-    fetch('http://localhost:3000/users').then(function (response) {
-        return response.json();
-    }).then(function (obj) {
-        console.log(obj);
-    }).catch(function (error) {
-        console.error('Something went wrong with retrieving users!.');
-        console.log(error);
+        fetch('http://localhost:3000/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(obj => {
+            console.log(obj);
+        })
+        .catch(error => {
+            console.error('Something went wrong with retrieving users!');
+            console.log(error);
+        });
     });
 });
-
-
-

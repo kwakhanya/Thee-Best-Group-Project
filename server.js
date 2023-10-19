96547e54e3fe7483af44cf0e3d4a45a6818a180f
Thabiso Matsaba <Thabisomatsaba@capaciti.org.za>
2023-10-19 11:59:31 +0200

    x

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ const corsOptions = {
 };
 
 
-erver.use(cors(corsOptions));  // Apply CORS middleware
+server.use(cors(corsOptions));  // Apply CORS middleware
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 const { app, db } = initializeFirebase();
